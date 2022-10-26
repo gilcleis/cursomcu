1af9698c3d58a6bd2a531d5112848adb1f4b9075
Gil <gilclei@gmail.com>
2022-10-26 17:06:39 -0300

    start project

---
+++ src/test/java/com/gilclei/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gilclei.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
