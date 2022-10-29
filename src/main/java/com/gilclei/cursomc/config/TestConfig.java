27b3e709aed50c12990dfa3c6bf6605d842bce36
Gil <gilclei@gmail.com>
2022-10-29 12:40:21 -0300

    instantiating Products

--- src/main/java/com/gilclei/cursomc/config/TestConfig.java
+++ src/main/java/com/gilclei/cursomc/config/TestConfig.java
@@ -8,7 +8,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.gilclei.cursomc.domain.Categoria;
+import com.gilclei.cursomc.domain.Produto;
 import com.gilclei.cursomc.repositories.CategoriaRepository;
+import com.gilclei.cursomc.repositories.ProdutoRepository;
 
 @Configuration
 @Profile("test")
@@ -17,14 +19,30 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository;
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
 	@Override
 	public void run(String... args) throws Exception {
 
-		Categoria cat1 = new Categoria(null, "Electronics");
-		Categoria cat2 = new Categoria(null, "Books");
-		Categoria cat3 = new Categoria(null, "Computers");
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Eletronicos");
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
 	}
 
 }
