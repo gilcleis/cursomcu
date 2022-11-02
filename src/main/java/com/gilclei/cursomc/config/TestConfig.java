b353a8f8c9b0f994d019a584a776843ce771c9ad
Gil <gilclei@gmail.com>
2022-11-02 12:24:55 -0300

    instantiating Cliente, TipoCliente, telefones e endereco

--- src/main/java/com/gilclei/cursomc/config/TestConfig.java
+++ src/main/java/com/gilclei/cursomc/config/TestConfig.java
@@ -9,10 +9,15 @@ import org.springframework.context.annotation.Profile;
 
 import com.gilclei.cursomc.domain.Categoria;
 import com.gilclei.cursomc.domain.Cidade;
+import com.gilclei.cursomc.domain.Cliente;
+import com.gilclei.cursomc.domain.Endereco;
 import com.gilclei.cursomc.domain.Estado;
 import com.gilclei.cursomc.domain.Produto;
+import com.gilclei.cursomc.domain.enums.TipoCliente;
 import com.gilclei.cursomc.repositories.CategoriaRepository;
 import com.gilclei.cursomc.repositories.CidadeRepository;
+import com.gilclei.cursomc.repositories.ClienteRepository;
+import com.gilclei.cursomc.repositories.EnderecoRepository;
 import com.gilclei.cursomc.repositories.EstadoRepository;
 import com.gilclei.cursomc.repositories.ProdutoRepository;
 
@@ -31,6 +36,12 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private ClienteRepository  clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -62,6 +73,15 @@ public class TestConfig implements CommandLineRunner {
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null,"Maria Silva","maria@gmail.com","999999999",TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("2222222","3333333"));
+		Endereco e1 = new Endereco(null,"rua das flores","10","casa","centro","42800000",cli1,c1);
+		Endereco e2 = new Endereco(null,"av matos","10","casa","centro","42800000",cli1,c2);
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.save(cli1);
+		enderecoRepository.saveAll(Arrays.asList(e1,e2));
 
 	}
 
---
+++ src/main/java/com/gilclei/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.gilclei.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gilclei.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
