be066e54147ba79e29f94a239f2cd6407f758811
Gil <gilclei@gmail.com>
2022-11-02 12:02:11 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/gilclei/cursomc/domain/Cliente.java
@@ -0,0 +1,123 @@
+package com.gilclei.cursomc.domain;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.gilclei.cursomc.domain.enums.TipoCliente;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String email;
+	private String cpfOuCnpj;
+	private Integer tipoCliente;
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Endereco> enderecos = new ArrayList<>();
+	
+	@ElementCollection
+	@CollectionTable(name = "telefones")
+	private Set<String> telefones = new HashSet<>();
+
+	public Cliente() {
+	}
+
+	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipoCliente) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cpfOuCnpj = cpfOuCnpj;
+		this.tipoCliente = tipoCliente.getCod();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpfOuCnpj() {
+		return cpfOuCnpj;
+	}
+
+	public void setCpfOuCnpj(String cpfOuCnpj) {
+		this.cpfOuCnpj = cpfOuCnpj;
+	}
+
+	public TipoCliente getTipoCliente() {
+		return TipoCliente.toEnum(tipoCliente);
+	}
+
+	public void setTipoCliente(TipoCliente tipoCliente) {
+		this.tipoCliente = tipoCliente.getCod();
+	}
+
+	public List<Endereco> getEnderecos() {
+		return enderecos;
+	}
+
+	public Set<String> getTelefones() {
+		return telefones;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", nome=" + nome + ", email=" + email + ", cpfOuCnpj=" + cpfOuCnpj
+				+ ", tipoCliente=" + tipoCliente + "]";
+	}
+
+}
---
+++ src/main/java/com/gilclei/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,37 @@
+package com.gilclei.cursomc.domain.enums;
+
+public enum TipoCliente {
+
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Juridica");
+
+	private Integer cod;
+	private String descricao;
+
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+
+	public Integer getCod() {
+		return cod;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public static TipoCliente toEnum(Integer cod) {
+		if (cod == null) {
+			return null;
+		}
+
+		for (TipoCliente x : TipoCliente.values()) {
+			if (cod.equals(x.getCod())) {
+				return x;
+			}
+
+		}
+		throw new IllegalArgumentException("Id inválido: " + cod);
+	}
+}
