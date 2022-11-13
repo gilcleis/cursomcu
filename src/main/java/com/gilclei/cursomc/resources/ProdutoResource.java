df879d1c71dbcf77e37db72929d39f6174b37149
Gil <gilclei@gmail.com>
2022-11-13 16:39:22 -0300

    Search for products by name and categories

--- src/main/java/com/gilclei/cursomc/resources/ProdutoResource.java
+++ src/main/java/com/gilclei/cursomc/resources/ProdutoResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +13,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gilclei.cursomc.domain.Produto;
+import com.gilclei.cursomc.dto.ProdutoDTO;
+import com.gilclei.cursomc.resources.utils.URL;
 import com.gilclei.cursomc.services.ProdutoService;
 
 @RestController
@@ -54,6 +58,22 @@ public class ProdutoResource {
 	public ResponseEntity<Produto> update(@PathVariable Integer id, @RequestBody Produto obj) {
 		obj = service.update(id, obj);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping(value = "/page")
+	public ResponseEntity<Page<ProdutoDTO>> findPage(
+			@RequestParam(value = "nome",defaultValue = "") String nome,
+			@RequestParam(value = "categorias",defaultValue = "") String categorias,
+			@RequestParam(value = "page",defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage",defaultValue = "24")Integer linesPerPage,
+			@RequestParam(value = "orderBy",defaultValue = "nome")String orderBy,
+			@RequestParam(value = "direction",defaultValue = "ASC")String direction
+			) {
+		String nomeDecoded = URL.decodeParam(nome);
+		List<Integer> ids = URL.decodeIntList(categorias);
+		Page<Produto> produtos = service.search(nomeDecoded,ids,page, linesPerPage, orderBy, direction);
+		Page<ProdutoDTO> listDTO = produtos.map(x-> new ProdutoDTO(x));
+		return ResponseEntity.ok().body(listDTO);
 	}
 
 }
--- src/main/java/com/gilclei/cursomc/services/ProdutoService.java
+++ src/main/java/com/gilclei/cursomc/services/ProdutoService.java
@@ -8,9 +8,14 @@ import javax.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.gilclei.cursomc.domain.Categoria;
 import com.gilclei.cursomc.domain.Produto;
+import com.gilclei.cursomc.repositories.CategoriaRepository;
 import com.gilclei.cursomc.repositories.ProdutoRepository;
 import com.gilclei.cursomc.services.exeptions.DatabaseException;
 import com.gilclei.cursomc.services.exeptions.IntegrityConstraintViolationException;
@@ -21,6 +26,9 @@ public class ProdutoService {
 
 	@Autowired
 	private ProdutoRepository repository;
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public Produto insert(Produto obj) {
 		try {
@@ -62,7 +70,12 @@ public class ProdutoService {
 
 	private void updateData(Produto entity, Produto obj) {
 		entity.setNome(obj.getNome());
-
+	}
+	
+	public Page<Produto> search(String nome, List<Integer> ids,Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repository.findDistinctByNomeContainingAndCategoriasIn(nome,categorias,pageRequest);
 	}
 
 }
