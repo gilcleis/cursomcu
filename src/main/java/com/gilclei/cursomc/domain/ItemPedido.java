7cbf16d2bc785e91faeefedc42db8a928d5c2fdb
Gil <gilclei@gmail.com>
2022-11-13 16:54:27 -0300

    order total and subtotal

--- src/main/java/com/gilclei/cursomc/domain/ItemPedido.java
+++ src/main/java/com/gilclei/cursomc/domain/ItemPedido.java
@@ -31,6 +31,10 @@ public class ItemPedido  implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
+	
+	public Double getSubTotal() {
+		return (preco-desconto)* quantidade;
+	}
 
 	public ItemPedidoPk getId() {
 		return id;
--- src/main/java/com/gilclei/cursomc/domain/Pedido.java
+++ src/main/java/com/gilclei/cursomc/domain/Pedido.java
@@ -57,6 +57,15 @@ public class Pedido implements Serializable {
 		this.cliente = cliente;
 		this.enderecoEntrega = enderecoEntrega;
 	}
+	
+	public Double getTotal() {
+		double soma = 0.0;
+		for(ItemPedido ip: itens) {
+			soma += ip.getSubTotal();
+		}
+		return soma;		
+		
+	}
 
 	public Integer getId() {
 		return id;
