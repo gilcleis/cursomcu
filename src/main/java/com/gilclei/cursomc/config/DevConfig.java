f9e2e955a5ac9defb0883112172245d488b0810f
Gil <gilclei@gmail.com>
2022-11-14 09:26:39 -0300

    Creating development profile

---
+++ src/main/java/com/gilclei/cursomc/config/DevConfig.java
@@ -0,0 +1,40 @@
+package com.gilclei.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.gilclei.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig implements CommandLineRunner {
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException  {
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		//metodo roda automaticamente
+
+	}
+
+}
