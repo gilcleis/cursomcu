d003ac154db698f63e31f7eba93b7dc7c884f0e7
Gil <gilclei@gmail.com>
2022-11-20 16:15:56 -0300

    implementing authorization

--- src/main/java/com/gilclei/cursomc/config/SecurityConfig.java
+++ src/main/java/com/gilclei/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.gilclei.cursomc.security.JWTAuthenticationFilter;
+import com.gilclei.cursomc.security.JWTAuthorizationFilter;
 import com.gilclei.cursomc.security.JWTUtil;
 
 @Configuration
@@ -57,6 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
--- src/main/java/com/gilclei/cursomc/security/JWTUtil.java
+++ src/main/java/com/gilclei/cursomc/security/JWTUtil.java
@@ -5,24 +5,51 @@ import java.util.Date;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
 public class JWTUtil {
-	
+
 	@Value("${jwt.secret}")
 	private String secret;
 
 	@Value("${jwt.expiration}")
 	private Long expiration;
-	
+
 	public String generateToken(String username) {
-		return Jwts.builder()
-				.setSubject(username)
-				.setExpiration(new Date(System.currentTimeMillis() + expiration))
-				.signWith(SignatureAlgorithm.HS512, secret.getBytes())
-				.compact();
+		return Jwts.builder().setSubject(username).setExpiration(new Date(System.currentTimeMillis() + expiration))
+				.signWith(SignatureAlgorithm.HS512, secret.getBytes()).compact();
+	}
+
+	public boolean tokenValido(String token) {
+		Claims claims = getClaims(token);
+		if (claims != null) {
+			String username = claims.getSubject();
+			Date expirationDate = claims.getExpiration();
+			Date now = new Date(System.currentTimeMillis());
+			if (username != null && expirationDate != null && now.before(expirationDate)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private Claims getClaims(String token) {
+		try {
+			return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJws(token).getBody();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	public String getUsername(String token) {
+		Claims claims = getClaims(token);
+		if (claims != null) {
+			return claims.getSubject();
+		}
+		return null;
 	}
 
 }
