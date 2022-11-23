df0752857998c6bfd28dd4c3dc31b42a5c3b8b4e
Gil <gilclei@gmail.com>
2022-11-23 14:07:43 -0300

    Refresh token

---
+++ src/main/java/com/gilclei/cursomc/resources/AuthResource.java
@@ -0,0 +1,30 @@
+package com.gilclei.cursomc.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gilclei.cursomc.security.JWTUtil;
+import com.gilclei.cursomc.security.UserSS;
+import com.gilclei.cursomc.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+	
+	@Autowired
+	private JWTUtil jwtUtil;
+	
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
+	}
+
+}
