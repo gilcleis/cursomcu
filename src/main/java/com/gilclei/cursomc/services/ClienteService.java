c56c95415beb0ee470a1bb64e44c0de15fd6ff7b
Gil <gilclei@gmail.com>
2022-12-28 16:00:44 -0300

    Bônus: ajustando tamanho da imagem

--- src/main/java/com/gilclei/cursomc/services/ClienteService.java
+++ src/main/java/com/gilclei/cursomc/services/ClienteService.java
@@ -54,6 +54,9 @@ public class ClienteService {
 
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
+	
+	@Value("${img.profile.size}")
+	private Integer size;
 
 	@Transactional
 	public Cliente insert(Cliente obj) {
@@ -139,6 +142,8 @@ public class ClienteService {
 		}
 		
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multipartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
 		String fileName = prefix + user.getId() + ".jpg";
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
 	}
